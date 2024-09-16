e10a3836a3d1fa8de6ea4e8676eaac8800c6ca84
Vitor Gabriel <sgvitor@outlook.com.br>
2024-09-15 21:20:39 -0300

    feat: create banner

---
+++ src/components/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CampoTexto = () => {
+    return (
+        <div>
+            <label>Nome:</label>
+            <input></input>
+        </div>
+    );
+};
+
+export default CampoTexto;
